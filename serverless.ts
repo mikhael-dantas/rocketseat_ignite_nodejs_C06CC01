d2fd4212a4d0d8fe7b4909fe487188ff25707664
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-05 23:57:48 -0300

    initial commit

---
+++ serverless.ts
@@ -0,0 +1,49 @@
+import type { AWS } from '@serverless/typescript';
+
+const serverlessConfiguration: AWS = {
+  service: 'rocketseat-ignite-nodejs-c06cc01',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild', 'serverless-offline'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+    },
+  },
+  // import the function via paths
+  functions: {
+    Hello: {
+      handler: 'src/functions/Hello.handler',
+      events: [
+        {
+          http: {
+            method: 'get',
+            path: '/',
+            cors: true
+          }
+        }
+      ]
+    }
+  },
+  package: { individually: true },
+  custom: {
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;

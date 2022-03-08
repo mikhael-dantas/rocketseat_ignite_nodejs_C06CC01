b2be84e630e4a00019c0c5b8b50905329bfb59e7
mikhael-dantas <mikhael-dcs@outlook.com>
2022-03-07 23:50:01 -0300

    adding comment

--- src/functions/Hello.ts
+++ src/functions/Hello.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from "aws-lambda"
 
+// making a hello world
 export const handler: APIGatewayProxyHandler = async (event) => {
    return {
       statusCode: 200,
